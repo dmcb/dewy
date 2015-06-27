039a7580b90b5d93dd82f5713ef9385e13ed3221
dmcb <d.mcburney@gmail.com>
2015-06-26 22:28:05 -0600

    Split projects into their own documents
    Storing all project data in the site document leads to a lot of
    redundant data.

--- server/dewey.py
+++ server/dewey.py
@@ -8,21 +8,30 @@ with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
     file.close()
 
-# Get all Drupal site directories and define sites
-site_directories = dict()
-
+# Get all Drupal sites, projects
+sites = dict()
+projects = dict()
 for uri in os.listdir(config['sites-root']):
 	directory = os.path.realpath(os.path.join(config['sites-root'], uri))
 
+	# Get sites
 	if os.path.isdir(directory) and os.path.split(directory)[1] != 'all' and os.path.split(directory)[1] != 'default':
 		# There may be multiple folders pointing at the same site, create a URI for each
-		if directory in site_directories:
-			site_directories[directory].details.uris.append(uri)
+		if directory in sites:
+			sites[directory].details.uris.append(uri)
 		else:
-			site_directories[directory] = Site(config['drupal-root'], directory, uri)
+			sites[directory] = Site(config['drupal-root'], directory, uri)
+
+		# Get list of projects, avoid duplicates
+		site_projects = sites[directory].get_projects()
+		for site_project in site_projects:
+			if site_projects[site_project]['path'] not in projects:
+				projects[site_projects[site_project]['path']] = site_projects[site_project]
 
 # Pass to Couch
 couch = couchdb.Server(config['couch-location'])
 db = couch['dewey']
-for directory, site in sorted(site_directories.items()):
+for directory, site in sites.items():
 	db.save({directory: site.details})
+for path, project in projects.items():
+	db.save({path: project})
--- server/dewey_site.py
+++ server/dewey_site.py
@@ -5,10 +5,12 @@ class Site:
 
 	def __init__(self, root, directory, uri):
 
-		os.chdir(directory)
+		self.root = root
+		self.directory = directory
+		os.chdir(self.directory)
 
 		# Get status
-		process = Popen(['drush', '--format=json', '--root=' + root, '--uri=' + uri, 'status'], stdout=PIPE, stderr=PIPE)
+		process = Popen(['drush', '--format=json', '--root=' + self.root, '--uri=' + uri, 'status'], stdout=PIPE, stderr=PIPE)
 		out, err = process.communicate()
 		self.details = json.loads(out)
 		self.details['uris'] = [uri]
@@ -18,16 +20,23 @@ class Site:
 		self.details['filesize'] = 0
 		self.details['privatefilecount'] = 0
 		self.details['privatefilesize'] = 0
-		for dir, subdir, files in os.walk(os.path.join(root, self.details['files'])):
+		for dir, subdir, files in os.walk(os.path.join(self.root, self.details['files'])):
 			for file in files:
 				self.details['filecount'] = self.details['filecount'] + 1
-				self.details['filesize'] += os.path.getsize(os.path.join(root, self.details['files']))
-		for dir, subdir, privatefiles in os.walk(os.path.join(root, self.details['private'])):
+				self.details['filesize'] += os.path.getsize(os.path.join(self.root, self.details['files']))
+		for dir, subdir, privatefiles in os.walk(os.path.join(self.root, self.details['private'])):
 			for privatefile in privatefiles:
 				self.details['privatefilecount'] = self.details['privatefilecount'] + 1
-				self.details['privatefilesize'] += os.path.getsize(os.path.join(root, self.details['private']))
+				self.details['privatefilesize'] += os.path.getsize(os.path.join(self.root, self.details['private']))
 
-		# Get projects
-		process = Popen(['drush', '--format=json', '--root=' + root, '--uri=' + uri, 'pm-info'], stdout=PIPE, stderr=PIPE)
+	def get_projects(self):
+
+		self.details['projects'] = []
+		os.chdir(self.directory)
+
+		process = Popen(['drush', '--format=json', '--root=' + self.root, '--uri=' + self.details['uris'][0], 'pm-info'], stdout=PIPE, stderr=PIPE)
 		out, err = process.communicate()
-		self.details['projects'] = json.loads(out)
+		projects = json.loads(out)
+		for project in projects:
+			self.details['projects'].append([projects[project]['path'], projects[project]['status']])
+		return projects
