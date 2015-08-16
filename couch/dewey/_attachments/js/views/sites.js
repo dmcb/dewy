79c63cfaea773f4093c8bed229338bbc31fb0a4b
dmcb <damcburn@ucalgary.ca>
2015-08-15 22:22:59 -0600

    Adds sorting of factor values in sites list

--- couch/dewey/_attachments/js/views/sites.js
+++ couch/dewey/_attachments/js/views/sites.js
@@ -13,10 +13,12 @@ $(function( $ ) {
 		template: _.template($('#sites').html()),
 
 		events: {
+			"click .headers li a" : "sortSites",
 		},
 
 		initialize: function() {
 			this.collection.bind('reset', this.render, this);
+			this.collection.bind('sort', this.render, this);
 			this.loadSites();
 		},
 		
@@ -56,7 +58,7 @@ $(function( $ ) {
 			// Render
 			this.collection.each(function(site) {
 				for (var factor in factors) {
-					site.attributes['relative_' + factor] = ((site.attributes[factor] - factors[factor]['minimum']) / factors[factor]['increment']) + 1;
+					site.attributes['relative_' + factor] = Math.round((site.attributes[factor] - factors[factor]['minimum']) / factors[factor]['increment']) + 1;
 					// Should there be no increment in the range because all sites are the same or there's only one, set relative values in the middle
 					if (factors[factor]['increment'] == 0) {
 						site.attributes['relative_' + factor] = 3;
@@ -77,6 +79,20 @@ $(function( $ ) {
 				group: true,
 				reduce: true
 			});
+        },
+
+		sortSites: function(e) {
+			var target = $(e.currentTarget);
+			var direction = target.attr("class");
+			var factor = target.parent().attr("class");
+			this.collection.comparator = function(model) {
+				if (direction == "up") {
+					return -model.get(factor);
+				} else {
+					return model.get(factor);
+				}
+			}
+			this.collection.sort();
         }
 	});
 });
