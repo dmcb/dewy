41f11bb57ce1ee000fc5d1e05250f659300da34b
dmcb <damcburn@ucalgary.ca>
2015-08-16 21:42:03 -0600

    Changes column values to be visual circles

--- couch/dewey/_attachments/js/views/sites.js
+++ couch/dewey/_attachments/js/views/sites.js
@@ -8,6 +8,30 @@ var Dewey = Dewey || {
 $(function( $ ) {
 	'use strict';
 
+	Dewey.Views.Site = Backbone.View.extend({
+		template: _.template($('#site').html()),
+
+		initialize: function() {
+			this.model.bind('reset', this.render, this);
+		},
+
+		render: function() {
+			$(this.el).html(this.template(this.model.attributes));
+
+			var factors = ['complexity', 'size', 'activity', 'health'];
+			var pointTemplate = _.template($('#point').html());
+			for (var factor in factors) {
+				var points_html = this.$('.' + factors[factor] + ' ' + '.points');
+				var points = this.model.get('relative_' + factors[factor])
+				for (var i=0; i<points; i++) {
+					points_html.append(pointTemplate);
+				}
+			}
+
+			return this
+		}
+	})
+
 	Dewey.Views.Sites = Backbone.View.extend({
 		el: "#content",
 		template: _.template($('#sites').html()),
@@ -52,7 +76,7 @@ $(function( $ ) {
 
 			// Determine buckets from range of factor values
 			for (var factor in factors) {
-				factors[factor]['increment'] = (factors[factor]['maximum'] - factors[factor]['minimum']) / 4;
+				factors[factor]['increment'] = (factors[factor]['maximum'] - factors[factor]['minimum']) / 9;
 			}
 
 			// Render
@@ -61,11 +85,15 @@ $(function( $ ) {
 					site.attributes['relative_' + factor] = Math.round((site.attributes[factor] - factors[factor]['minimum']) / factors[factor]['increment']) + 1;
 					// Should there be no increment in the range because all sites are the same or there's only one, set relative values in the middle
 					if (factors[factor]['increment'] == 0) {
-						site.attributes['relative_' + factor] = 3;
+						// var tpl = _.template("<h1>LOL</h1>");
+						site.attributes['relative_' + factor] = 10;
 					}
 				}
 
-				sites.append(siteTemplate(site.attributes));
+				var view = new Dewey.Views.Site({
+					model: site
+				});
+				sites.append(view.render().el);
 			});
 		},
 		   
