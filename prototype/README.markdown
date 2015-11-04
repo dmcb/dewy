1010e3adcca8d7d8623cb8ad36225e1874cfb43b
Derek McBurney <d.mcburney@icloud.com>
2015-11-03 22:44:30 -0700

    Integrates static HTML with Expressjs app
    Added mustache-express as the view engine and moved the static html
    files to mustache templates.

---
+++ prototype/README.markdown
@@ -0,0 +1,14 @@
+## Installation
+
+* Install dependencies:
+
+		npm install
+		
+* Run node:
+
+		node app.js
+		
+
+## Usage
+
+* Go to localhost:3000 to visit the site
--- prototype/routes/index.js
+++ prototype/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dewy' });
+});
+
+router.get('/filter', function(req, res, next) {
+  res.render('filter', { title: 'Dewy' });
 });
 
 module.exports = router;
