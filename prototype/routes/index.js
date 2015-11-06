2967544374a898eeae76b2575b98072439113646
Derek McBurney <d.mcburney@icloud.com>
2015-11-05 22:56:11 -0700

    Changes index route to sites

--- prototype/routes/index.js
+++ prototype/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dewy' });
+router.get('/sites', function(req, res, next) {
+  res.render('sites', { title: 'Dewy' });
 });
 
 router.get('/filter', function(req, res, next) {
