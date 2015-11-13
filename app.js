d6d0830b8a57058ca96e3b4cb8bb02fee4af441d
Derek McBurney <d.mcburney@icloud.com>
2015-11-12 22:34:02 -0700

    Cleans up comments and route files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Packages
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,10 +7,13 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var session = require('express-session');
 
+// Router handlers
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+// Register express
 var app = express();
+
+// Define handlebars and helpers
 var hbs = exphbs.create({
   defaultLayout: 'index',
   helpers: {
@@ -23,13 +27,12 @@ var hbs = exphbs.create({
   }
 });
 
-// Register '.handlebars' extension with The Mustache Express
+// Register '.handlebars' extension
 app.engine('.handlebars', hbs.engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.handlebars');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,19 +45,16 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+// 404
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// Development error handler
+// Will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -65,8 +65,8 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler
+// No stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
