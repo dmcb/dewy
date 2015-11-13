952f8b61d3ebba4d98f3583f500ca36c0d8d4e8a
Derek McBurney <d.mcburney@icloud.com>
2015-11-12 23:32:38 -0700

    Adds styling for sign on form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/signon', function(req, res, next) {
   res.render('signon', { 
-    title: 'Dewy'
+    title: 'Dewy',
+    layout: 'splash'
   });
 });
 
