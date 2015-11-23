7adf94cf2830a5a869a685a4d3e777cbaf4178bb
Derek McBurney <d.mcburney@icloud.com>
2015-11-22 22:39:46 -0700

    Removes old site data

--- models/sites.js
+++ models/sites.js
@@ -5,24 +5,24 @@ exports.getByFilter = function(user, filter) {
 
 sites = [
   {
-    title: 'Schulich',
-    base_url: 'schulich.ucalgary.ca',
+    title: 'Photography Blog',
+    base_url: 'photographybyderek.ca/blog',
     complexity: 3.53,
     size: 10,
     activity: 4.42,
     health: 1
   },
   {
-    title: 'Haskayne',
-    base_url: 'haskayne.ucalgary.ca',
+    title: 'Derek McBurney',
+    base_url: 'derekmcburney.com',
     complexity: 1,
     size: 4.17,
     activity: 7.35,
     health: 6.4
   },
   {
-    title: 'Science',
-    base_url: 'ucalgary.ca/science',
+    title: 'my world, my choice!',
+    base_url: 'myworldmychoice.org',
     complexity: 1,
     size: 6.12,
     activity: 4.92,
