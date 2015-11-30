6871c1d61ff1cd46594be73ce26116f247780084
Derek McBurney <d.mcburney@icloud.com>
2015-11-29 22:42:28 -0700

    Adds sites template and controller
    Neither is done, but the template implements sorting and search on
    sites.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -22,5 +22,50 @@ controllers.controller('signonController', ['$scope', '$http',
 
 controllers.controller('sitesController', ['$scope', '$http',
 	function ($scope, $http) {
-		$scope.message = 'omg';
+		$scope.getNumber = function(number) {
+			return new Array(Math.round(number));
+		}
+		$scope.changeSorting = function(column) {
+            var sort = $scope.sort;
+ 
+            if (sort.column == column) {
+                sort.descending = !sort.descending;
+            } else {
+                sort.column = column;
+                sort.descending = false;
+            }
+        };
+		$scope.sort = {
+			column: 'title',
+			descending: false
+		};
+		// $http.get('http://dewy.io/api/sites').success(function(data) {
+		// 	$scope.sites = data;
+		// });
+		$scope.sites = [
+			{
+				title: 'Photography Blog',
+				base_url: 'photographybyderek.ca/blog',
+				complexity: 3.53,
+				size: 10,
+				activity: 4.42,
+				health: 1
+			},
+			{
+				title: 'Derek McBurney',
+				base_url: 'derekmcburney.com',
+				complexity: 1,
+				size: 4.17,
+				activity: 7.35,
+				health: 6.4
+			},
+			{
+				title: 'my world, my choice!',
+				base_url: 'myworldmychoice.org',
+				complexity: 1,
+				size: 6.12,
+				activity: 4.92,
+				health: 4.55
+			}
+		];
 }]);
