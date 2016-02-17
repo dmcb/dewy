ebd37d8f0f4ca151603d2289cd414a4bdb5394aa
Derek McBurney <d.mcburney@icloud.com>
2016-02-16 19:10:13 -0700

    Refactors proxy API auth routes to use same logic
    Sign on and sign up talk to different API end points but both return a
    token for authentication.

--- routes/auth.js
+++ routes/auth.js
@@ -2,65 +2,50 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config');
 var jwt = require('jwt-simple');
+var app = express();
 
-router.post('/signon', function(req, res) {
-    var endPoint = 'http://api.dewy.io/1.0/oauth/token';
-    var encodedClient = new Buffer(config.client.client_id + ':' + config.client.client_secret).toString('base64');
-    var request = require('request');
-    request({
-        uri: endPoint,
-        method: req.method,
-        body: 'grant_type=password&username=' + req.body.username + '&password=' + req.body.password,
-        headers: {
-            'Authorization': 'Basic ' + encodedClient,
-            'Content-Type': 'application/x-www-form-urlencoded'
+authenticateWithDewy = function() {
+    return function(req, res, next) {
+        if (req.url == '/signon') {
+            var body = 'grant_type=password&username=' + req.body.username + '&password=' + req.body.password;
+            var endPoint = 'http://api.dewy.io/1.0/oauth/token';
         }
-    }, function(error, response, body) {
-        if (error) {
-            res.status(response.statusCode).send(error);
+        else if (req.url == '/signup') {
+            var body = 'grant_type=password&username=' + req.body.username + '&password=' + req.body.password + '&email=' + req.body.email;
+            var endPoint = 'http://api.dewy.io/1.0/users';
         }
-        else {
-            if (response.statusCode == '200') {
-                var token = jwt.encode(body, config.jwt.secret);
-                res.send(token);             
+        var encodedClient = new Buffer(config.client.client_id + ':' + config.client.client_secret).toString('base64');
+        var request = require('request');
+        request({
+            uri: endPoint,
+            body: body,
+            method: 'post',
+            headers: {
+                'Authorization': 'Basic ' + encodedClient,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                res.status(response.statusCode).end(error);
             }
             else {
-                res.status(response.statusCode).end();
+                if (response.statusCode == '200') {
+                    var token = jwt.encode(body, config.jwt.secret);
+                    res.end(token);             
+                }
+                else if (response.statusCode == '400') {
+                    res.status(response.statusCode).end(body);
+                }
+                else {
+                    res.status(response.statusCode).end();
+                }
             }
-        }
-    });
-});
+            next();
+        });
+    }
+}
 
-router.post('/signup', function(req, res) {
-    var endPoint = 'http://api.dewy.io/1.0/users';
-    var encodedClient = new Buffer(config.client.client_id + ':' + config.client.client_secret).toString('base64');
-    var request = require('request');
-    request({
-        uri: endPoint,
-        method: req.method,
-        body: 'grant_type=password&username=' + req.body.username + '&password=' + req.body.password + '&email=' + req.body.email,
-        headers: {
-            'Authorization': 'Basic ' + encodedClient,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }, function(error, response, body) {
-        if (error) {
-            res.status(response.statusCode).send(error);
-        }
-        else {
-            if (response.statusCode == '200') {
-                console.log(body);
-                var token = jwt.encode(body, config.jwt.secret);
-                res.send(token);             
-            }
-            else if (response.statusCode == '400') {
-                res.status(response.statusCode).send(body);
-            }
-            else {
-                res.status(response.statusCode).end();
-            }
-        }
-    });
-});
+router.post('/signon', authenticateWithDewy(), function(req, res) {});
+router.post('/signup', authenticateWithDewy(), function(req, res) {});
 
 module.exports = router;
