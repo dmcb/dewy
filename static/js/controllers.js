5542cc2e4a8d3e132e54f12c511679275bf15170
Derek McBurney <d.mcburney@icloud.com>
2016-06-29 00:25:38 -0600

    Refactors CSS
    CSS broken down into respective partials and layouts. Moved a lot of
    breakpoint specific rules into applicable sections. Created a lot of
    reuse between sections.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -668,9 +668,29 @@ controllers.controller('overviewUsersController', ['$scope',
 	function ($scope) {
 }]);
 
-controllers.controller('subscriptionController', ['$scope', '$timeout', '$rootScope', 'userFactory', 'authService',
-	function ($scope, $timeout, $rootScope, userFactory, authService) {
-		
+controllers.controller('subscriptionController', ['$scope', '$timeout', '$rootScope', 'userFactory', 'authService', 'stripe',
+	function ($scope, $timeout, $rootScope, userFactory, authService, stripe) {
+		$scope.charge = function () {
+			return stripe.card.createToken($scope.payment.card)
+				.then(function (response) {
+					console.log('token created for card ending in ', response.card.last4);
+					var payment = angular.copy($scope.payment);
+					payment.card = void 0;
+					payment.token = response.id;
+					return $http.post('https://yourserver.com/payments', payment);
+				})
+				.then(function (payment) {
+					console.log('successfully submitted payment for $', payment.amount);
+				})
+				.catch(function (err) {
+					if (err.type && /^Stripe/.test(err.type)) {
+						console.log('Stripe error: ', err.message);
+					}
+					else {
+						console.log('Other error occurred, possibly with your API', err.message);
+					}
+				});
+		};
 }]);
 
 controllers.controller('verifyController', ['$scope', 'verifyData',
