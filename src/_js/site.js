5a3087d1a761ec540090fe5dbd7d5396d562afab
Derek McBurney <d.mcburney@icloud.com>
2016-09-06 23:41:40 -0600

    Changes menu to slide out, and close when changing views

--- src/_js/site.js
+++ src/_js/site.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-	// Top navigation drop down toggle
-	$('header .drop').click(function(e) {
-		e.stopPropagation();
-		e.preventDefault();
+	// Top navigation drop down menu toggle
+	$('header .drop').on('click', function(event) {
+		event.stopPropagation();
+		event.preventDefault();
 		$(this).parent().toggleClass('dropped');
 	});
 
-	$('html').click(function() {
+	$('html').on('click', function() {
 		$('header .drop').parent().removeClass('dropped');
 	});
+
+    // Mobile menu toggle
+    $('#menu-toggle').on('click', function() {
+        $('header nav .menu').slideToggle(200);
+    });
+
+    $('header nav .menu a').on('click', function() {
+        $('header nav .menu').slideUp(0);
+    });
 });
